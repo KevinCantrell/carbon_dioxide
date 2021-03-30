720422007927fd3e4e5a66a34cf1c0572846c6e5
torizott <zottarel21@up.edu>
2021-03-30 12:21:24 -0700

    labeling code

--- co2.py
+++ co2.py
@@ -3,13 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+#pulls data from the file
 dfCarbonDioxide=pd.read_table('co2_mlo.txt',delimiter=r"\s+",skiprows=151)
+#setting up data point that is a combination of points relfected by time for x axis
 dfCarbonDioxide['date']=pd.to_datetime(dfCarbonDioxide[['year', 'month', 'day', 'hour', 'minute', 'second']])
+#setting up boolean data points for y axis of carbon value
 boolMissing=dfCarbonDioxide['value']==-999.99
 dfCarbonDioxide[boolMissing]=np.nan
 dfCarbonDioxide=dfCarbonDioxide.dropna()
 dfCarbonDioxide=dfCarbonDioxide.reset_index(drop=True)
 
+#creating figure
 fig, ax = plt.subplots(figsize=(12,8))
+#plotting x and y axis
 ax.plot(dfCarbonDioxide['date'],dfCarbonDioxide['value'],'ok')
 ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
