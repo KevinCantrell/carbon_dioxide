ff779f4e0fd6d7002ec352d138715931c5019a12
tlow215 <65463502+tlow215@users.noreply.github.com>
2021-03-31 16:55:45 -0700

    Update co2.py
    Added Polyfit

--- co2.py
+++ co2.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+def fitPly(x,c2,c1,c0):
+    y=x**2*c2+x*c1+c0
+    return y
+
 #reads file and stores it as pd dataframe
 dfCarbonDioxide=pd.read_table('co2_mlo.txt',delimiter=r"\s+",skiprows=151)
 #converts date and time data into datetime format
@@ -20,3 +24,11 @@ dfCarbonDioxide=dfCarbonDioxide.reset_index(drop=True)
 fig, ax = plt.subplots(figsize=(12,8))
 ax.plot(dfCarbonDioxide['date'],dfCarbonDioxide['value'],'ok')
 ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+
+startDate=min(dfCarbonDioxide['date'])
+timeElapsed=dfCarbonDioxide['date']-startDate
+daysSinceStart=timeElapsed.dt.days
+
+fitCoeffs=np.polyfit(daysSinceStart,dfCarbonDioxide['value'],2)
+fitCO2=fitPly(daysSinceStart,fitCoeffs[0],fitCoeffs[1],fitCoeffs[2])
+ax.plot(dfCarbonDioxide['date'],fitCO2,'-r')
