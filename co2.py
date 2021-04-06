e1bf8a6b7b86d4dc0cf71b4d655efd8526beadd8
mwawai21 <81767923+mwawai21@users.noreply.github.com>
2021-04-06 11:34:40 -0700

    Alevelwk10

--- co2.py
+++ co2.py
@@ -2,9 +2,19 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from scipy.optimize import curve_fit
 
 def fitPly(x,c2,c1,c0):
     y=x**2*c2+x*c1+c0
+    return y
+
+def fitOsc(x,amp,period,offset):
+    y=amp/2 * np.cos((x + offset)  * 2 * np.pi/period)
+    #y=amp*np.cos(period*(x+offset))
+    return y
+
+def fitAll(x,c2,c1,c0,amp,period,offset):
+    y=fitPly(x,c2,c1,c0)+fitOsc(x,amp,period,offset)
     return y
 
 dfCarbonDioxide=pd.read_table('co2_mlo.txt',delimiter=r"\s+",skiprows=151)
@@ -25,9 +35,17 @@ timeElapsed=dfCarbonDioxide['date']-startDate
 daysSinceStart=timeElapsed.dt.days
 
 fitCoeffs=np.polyfit(daysSinceStart,dfCarbonDioxide['value'],2)
-fitCO2=fitPly(daysSinceStart,fitCoeffs[0],fitCoeffs[1],fitCoeffs[2])
+popt,pcov=curve_fit(fitAll,daysSinceStart,dfCarbonDioxide['value'] )
+fitCO2=fitAll(daysSinceStart,fitCoeffs[0],fitCoeffs[1],fitCoeffs[2],8,365.25,200)
 ax.plot(dfCarbonDioxide['date'],fitCO2,'-r')
 
-#fig,axResidual=plt.subplots()
-#axResidual.plot(dfCarbonDioxide['date'],dfCarbonDioxide['value']-predicted,'ok')
+residuals=fitCO2-dfCarbonDioxide['value']
+figRes,axRes=plt.subplots(figsize=(12,8))
+axRes.plot(dfCarbonDioxide['date'],residuals)
+axRes.plot(daysSinceStart,residuals)
 
+
+stdError=np.sqrt(np.sum(residuals**2)/(len(dfCarbonDioxide['value'])-len(fitCoeffs)))
+
+#pyplOscillationCalc=fitOsc(daysSinceStart,8,365.25,200)
+#axRes.plot(daysSinceStart,OscillationCalc)
