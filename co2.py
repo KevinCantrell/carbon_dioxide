34e2c834453f5c5c588fdfc1523ba7ae21099767
Jonnie Metcalfe <metcalfe24@up.edu>
2024-03-28 12:42:01 -0700

    fitto2ndorderpolynomial

--- co2.py
+++ co2.py
@@ -4,8 +4,13 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from scipy.optimize import curve_fit
 from scipy import stats
+
+def CalcCO2Poly(days,c2,c1,c0):
+    co2calc=c2*days**2+c1*days+c0
+    return co2calc
+
  
-dfCarbonDioxide=pd.read_table('co2_mlo_surface-insitu_1_ccgg_DailyData.txt',delimiter=r"\s+",skiprows=150)
+dfCarbonDioxide=pd.read_table('co2_mlo_surface-insitu_1_ccgg_DailyData.txt',delimiter=r"\s+",skiprows=158)
 dfCarbonDioxide['date']=pd.to_datetime(dfCarbonDioxide[['year', 'month', 'day', 'hour', 'minute', 'second']])
 boolMissing=dfCarbonDioxide['value']==-999.99
 dfCarbonDioxide[boolMissing]=np.nan
@@ -17,6 +22,14 @@ ax.plot(dfCarbonDioxide['date'],dfCarbonDioxide['value'],'.k')
 ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
 
 
+startDate=min(dfCarbonDioxide['date'])
+timeElapsed=dfCarbonDioxide['date']-startDate
+daysSinceStart=timeElapsed.dt.days
+
+fitPoly= np.polyfit(daysSinceStart,dfCarbonDioxide['value'],2)
+
+co2fitPoly=CalcCO2Poly(daysSinceStart,fitPoly[0],fitPoly[1],fitPoly[2])
+ax.plot(dfCarbonDioxide["date"],co2fitPoly,"-r")
 
 
 
