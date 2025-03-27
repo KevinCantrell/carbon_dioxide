6aac53974be46f7ba0ba631ea32d57320a13e04a
ERobinson888 <robinjas25@up.edu>
2025-03-27 12:42:45 -0700

    made the red line
    shortened names to CD for carbon dioxide, made a function for the polynomial, calculated dates from start point.

--- co2.py
+++ co2.py
@@ -4,19 +4,41 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from scipy.optimize import curve_fit
 from scipy import stats
- 
-dfCarbonDioxide=pd.read_table('co2_mlo_surface-insitu_1_ccgg_DailyData.txt',delimiter=r"\s+",skiprows=158)
-dfCarbonDioxide['date']=pd.to_datetime(dfCarbonDioxide[['year', 'month', 'day', 'hour', 'minute', 'second']])
-boolMissing=dfCarbonDioxide['value']==-999.99
-dfCarbonDioxide[boolMissing]=np.nan
-dfCarbonDioxide=dfCarbonDioxide.dropna()
-dfCarbonDioxide=dfCarbonDioxide.reset_index(drop=True)
 
+def CDfunc(x, y1, y2, y3):
+    
+    CD = y1*(x**2)+(y2*x)+y3
+    return CD
+
+dfCD=pd.read_table('co2_mlo_surface-insitu_1_ccgg_DailyData.txt',delimiter=r"\s+",skiprows=158)
+dfCD['date']=pd.to_datetime(dfCD[['year', 'month', 'day', 'hour', 'minute', 'second']])
+boolMissing=dfCD['value']==-999.99
+dfCD[boolMissing]=np.nan
+dfCD=dfCD.dropna()
+dfCD=dfCD.reset_index(drop=True)
+
+#dates from start, adjusts x-axis based on this value
+startD=min(dfCD['date'])
+timeElapsed=dfCD['date']-startD
+dSStart=timeElapsed.dt.days
+
+#plots stuff
 fig, ax = plt.subplots(figsize=(12,8))
-ax.plot(dfCarbonDioxide['date'],dfCarbonDioxide['value'],'.k')
+ax.plot(dfCD['date'],dfCD['value'],'.k')
 ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
 
+# want a second-order polynomial
+#using scipy optimize.curve_fit
+CD_Pfit= np.polyfit(dSStart,dfCD['value'],2)
 
+CD_calc = CDfunc(dSStart,CD_Pfit[0],CD_Pfit[1],CD_Pfit[2])
 
+#CD_Cfit= curve_fit(CDfunc, dfCD['date'], dfCD['value'] )
 
+#plot
+#red line
+ax.plot(dfCD['date'], CD_calc, '-r')
 
+#ax.plot(dfCD['date'], CD_Cfit, '-g')
+
+#difference between red line and black points
