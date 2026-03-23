0fb7ac57d4663c9a8c811dcf4a7ace48e1939a9f
Vivian <martinv26@up.edu>
2026-03-23 16:20:12 -0700

    Update co2.py
    Model of CO2 with best fit line.

--- co2.py
+++ co2.py
@@ -5,8 +5,14 @@ import matplotlib.dates as mdates
 from scipy.optimize import curve_fit
 from scipy import stats
  
+def oscFunc(x, amp, offset, period):
+    return amp/2 * np.cos((x + offset)  * 2 * np.pi/period)
+def calcCO2(days, c2, c1, c0,amp, offset, period): 
+    return days**2 *c2 + days *c1 + c0 + oscFunc(days, amp, offset, period)
+
 '''reads a local file, skips 158rows, eliminate none real numbers, recalculates the index'''
-dfCarbonDioxide=pd.read_table('co2_mlo_surface-insitu_1_ccgg_DailyData.txt',delimiter=r"\s+",skiprows=158)
+
+dfCarbonDioxide=pd.read_table(r"co2_mlo_surface-insitu_1_ccgg_DailyData.txt",delimiter=r"\s+",skiprows=158)
 dfCarbonDioxide['date']=pd.to_datetime(dfCarbonDioxide[['year', 'month', 'day', 'hour', 'minute', 'second']])
 boolMissing=dfCarbonDioxide['value']==-999.99
 ''''data munging to make it usable'''
@@ -32,7 +38,14 @@ co2fit = dfCarbonDioxide['value']- co2
 fig2, az = plt.subplots()
 az.plot(dfCarbonDioxide['date'], co2fit)
 
-amp = 12
+amp = 8
 period = 365
-'''y = 12*np.cos(dfCarbonDioxide['date']/period)'''
 
+yCalc = oscFunc(daysSinceStart, amp, 0, period)
+az.plot(dfCarbonDioxide['date'], yCalc, color= 'red')
+
+coefs,cov= curve_fit(calcCO2, daysSinceStart, dfCarbonDioxide['value'], p0=[polyfit[0], polyfit[1], polyfit[2], amp, 0, period])
+
+yBestFitLine = calcCO2(daysSinceStart, coefs[0],coefs[1],coefs[2],coefs[3],coefs[4], coefs[5])
+
+ax.plot(dfCarbonDioxide['date'],yBestFitLine, color = 'red') 
